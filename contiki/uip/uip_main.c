e0e6f5c5e5ba0666a0d5b382f00ef7f0a36d1c45
adamdunkels <adamdunkels>
2003-08-12 21:14:21 +0000

    Removed old junk, fixed a bug in the IP address translation code which caused erroneous IP address lookups

--- contiki/uip/uip_main.c
+++ contiki/uip/uip_main.c
@@ -31,7 +31,7 @@
  *
  * This file is part of the uIP TCP/IP stack.
  *
- * $Id: uip_main.c,v 1.3 2003/04/25 08:48:57 adamdunkels Exp $
+ * $Id: uip_main.c,v 1.4 2003/08/12 21:14:21 adamdunkels Exp $
  *
  */
 
@@ -39,8 +39,6 @@
 /* uip_main.c: initialization code and main event loop. */
 
 #define NULL (void *)0
-
-
 
 #include "uip.h"
 #include "uip_arp.h"
@@ -56,115 +54,11 @@
 static u8_t i, arptimer;
 static u16_t start, current;
 
-#if 0
-static void idle(void);
-static struct dispatcher_proc p =
-  {DISPATCHER_PROC("uIP TCP/IP stack", idle, NULL, NULL)};
-static ek_id_t id;
-#endif /* 0 */
-
-#if 0
-/*-----------------------------------------------------------------------------------*/
-static void
-timer(void)
-{
-  for(i = 0; i < UIP_CONNS; ++i) {
-    uip_periodic(i);
-    if(uip_len > 0) {
-#ifdef WITH_TFE
-      uip_arp_out();
-      cs8900a_send();
-#endif /* WITH_TFE */
-#ifdef WITH_RS232
-      rs232dev_send();
-#endif /* WITH_RS232 */
-    }
-  }
-
-  for(i = 0; i < UIP_UDP_CONNS; i++) {
-    uip_udp_periodic(i);
-    /* If the above function invocation resulted in data that
-       should be sent out on the network, the global variable
-       uip_len is set to a value > 0. */
-    if(uip_len > 0) {
-#ifdef WITH_TFE
-      uip_arp_out();
-      cs8900a_send();
-#endif /* WITH_TFE */
-#ifdef WITH_RS232
-      rs232dev_send();
-#endif /* WITH_RS232 */
-    }
-  }   
-}
-#endif /* 0 */
 /*-----------------------------------------------------------------------------------*/
 void
 uip_main_init(void)
 {
-#if 0
-  u16_t ipaddr[2];
-
-  id = dispatcher_start(&p);
-  
-  arptimer = 0;
-  start = ek_clock();
-#endif /* 0 */
 }
-/*-----------------------------------------------------------------------------------*/
-#if 0
-static void
-idle(void)
-{
-#ifdef WITH_TFE
-  /* Poll Ethernet device to see if there is a frame avaliable. */
-  uip_len = cs8900a_poll();
-  if(uip_len > 0) {
-    /* A frame was avaliable (and is now read into the uip_buf), so
-       we process it. */ 
-    if(BUF->type == htons(UIP_ETHTYPE_IP)) {
-      uip_arp_ipin();
-      uip_len -= sizeof(struct uip_eth_hdr);
-      uip_input();
-      /* If the above function invocation resulted in data that
-	 should be sent out on the network, the global variable
-	 uip_len is set to a value > 0. */
-      if(uip_len > 0) {
-	uip_arp_out();
-	cs8900a_send();
-      }
-    } else if(BUF->type == htons(UIP_ETHTYPE_ARP)) {
-      uip_arp_arpin();
-      /* If the above function invocation resulted in data that
-	 should be sent out on the network, the global variable
-	 uip_len is set to a value > 0. */	
-      if(uip_len > 0) {
-	cs8900a_send();
-      }
-    }
-  }
-#endif /* WITH_TFE */
-  
-#ifdef WITH_RS232
-  uip_len = rs232dev_poll();
-  if(uip_len > 0) {
-    uip_process(UIP_DATA);
-    if(uip_len > 0) {
-      rs232dev_send();
-    }
-  }
-#endif /* WITH_RS232 */
-
-  /* Check the clock so see if we should call the periodic uIP
-     processing. */
-  current = ek_clock();
-
-  if((current - start) >= CLK_TCK/2) {
-    timer();
-    start = current;
-  }    
-}
-#endif /* 0 */
 /*-----------------------------------------------------------------------------------*/
 unsigned char
 uip_main_ipaddrconv(char *addrstr, unsigned char *ipaddr)
@@ -187,7 +81,7 @@ uip_main_ipaddrconv(char *addrstr, unsigned char *ipaddr)
 	*ipaddr = tmp;
 	++ipaddr;
 	tmp = 0;
-      } else if(c >= '0' || c <= '9') {
+      } else if(c >= '0' && c <= '9') {
 	tmp = (tmp * 10) + (c - '0');
       } else {
 	return 0;
