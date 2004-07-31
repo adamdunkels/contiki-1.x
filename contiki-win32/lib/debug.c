decdd4f990a1bc4bb43a42af511057dba491b0c4
oliverschmidt <oliverschmidt>
2004-07-31 14:53:19 +0000

    Improved trace output facility.

--- contiki-win32/lib/debug.c
+++ contiki-win32/lib/debug.c
@@ -1,8 +1,22 @@
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
+#include <stdio.h>
 
 void
-debug_print(char *s)
+debug_printf(char *format, ...)
 {
-  OutputDebugString(s);
+  va_list argptr;
+  char buffer[1024];
+
+  va_start(argptr, format);
+  vsprintf(buffer, format, argptr);
+  va_end(argptr);
+
+  OutputDebugString(buffer);
+}
+
+void
+uip_log(char *message)
+{
+  debug_printf("%s\n", message);
 }
