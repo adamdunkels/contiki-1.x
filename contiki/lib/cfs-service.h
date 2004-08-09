c27f328ed2d30d2a9347abf1037cd380e70cae49
adamdunkels <adamdunkels>
2004-08-09 20:42:26 +0000

    Updated CFS API to include directory functions

--- contiki/lib/cfs-service.h
+++ contiki/lib/cfs-service.h
@@ -2,19 +2,31 @@
 #define __CFS_SERVICE_H__
 
 #include "ek-service.h"
+#include "cfs.h"
+
 
 #define CFS_SERVICE_VERSION 0x01
 #define CFS_SERVICE_NAME "Filesystem"
 
-struct cfs_service_interface {
-  u8_t version;
-  int  (* open)(char *name, int flags);
-  void (* close)(int fd);
-  int  (* read)(int fd, char *buf, int len);
-  int  (* write)(int fd, char *buf, int len);
+struct cfs_dir {
+  unsigned char dummy_space[32];
+};
 
-  int  (* dio_read)(char *buf, int track, int sector);
-  int  (* dio_write)(char *buf, int track, int sector);
+struct cfs_dirent {
+  unsigned char name[32];
+  unsigned int size;
+};
+
+struct cfs_service_interface {
+  unsigned char version;
+  int  (* open)(const char *name, int flags);
+  void (* close)(int fd);
+  int  (* read)(int fd, char *buf, unsigned int len);
+  int  (* write)(int fd, char *buf, unsigned int len);
+
+  int  (* opendir)(struct cfs_dir *dir, const char *name);
+  int  (* readdir)(struct cfs_dir *dir, struct cfs_dirent *dirent);
+  int  (* closedir)(struct cfs_dir *dir);
 };
 
 #endif /* __CFS_SERVICE_H__ */
