e9ef4cb6ca517683fe9b658f322755b4c6298a59
adamdunkels <adamdunkels>
2004-08-11 21:23:06 +0000

    Added packet service name TAP

--- contiki-gtk/uip/tapdev-service.c
+++ contiki-gtk/uip/tapdev-service.c
@@ -12,7 +12,7 @@ static const struct packet_service_state state =
 
 EK_EVENTHANDLER(eventhandler, ev, data);
 EK_POLLHANDLER(pollhandler);
-EK_PROCESS(proc, PACKET_SERVICE_NAME, EK_PRIO_NORMAL,
+EK_PROCESS(proc, PACKET_SERVICE_NAME ": TAP", EK_PRIO_NORMAL,
 	   eventhandler, pollhandler, (void *)&state);
 
 /*---------------------------------------------------------------------------*/
@@ -24,8 +24,6 @@ EK_PROCESS_INIT(tapdev_service_init, arg)
 static void
 output(u8_t *hdr, u16_t hdrlen, u8_t *data, u16_t datalen)
 {
-  printf("packet-service: output (%p, %d) (%p, %d)\n",
-	 hdr, hdrlen, data, datalen);
   tapdev_send();
 }
 /*---------------------------------------------------------------------------*/
@@ -34,6 +32,7 @@ EK_EVENTHANDLER(eventhandler, ev, data)
   printf("packet-service: event %d\n", ev);
   switch(ev) {
   case EK_EVENT_INIT:
+  case EK_EVENT_REPLACE:
     tapdev_init();
     break;
   case EK_EVENT_REQUEST_REPLACE:
