eaea2d833bd0af253b6c8642e6793967975a4769
adamdunkels <adamdunkels>
2004-08-11 21:19:06 +0000

    Purified protothread interface, moved socket functionality into uip/socket.h file

--- contiki/lib/pt.h
+++ contiki/lib/pt.h
@@ -1,157 +1,133 @@
-/* pt: TCP/IP application "protothread" (proto meaning both
-   "primitive" and "protocol") programming based on "local
-   continuations". */ 
+/**
+ * \defgroup protothreads Protothreads
+ *
+ * Protothreads are a primitive form of threads that can be used to
+ * implement sequential control flow and blocked waiting in
+ * event-driven environments. a non-threaded environment. Unlike
+ * ordinary threads, a protothread only provides linear control flow
+ * in a single C function, and not the entire execution context. With
+ * protothreads, local variables are not stored across a
+ * yield. Furthermore, it is not possible to yield a protothread
+ * inside a deep function call tree. (This is not entirely true, the
+ * PT_WAIT_COND() function can be used for this.)
+ *
+ * Protothreads are very cheap to implement 
+ within a single function must manage its own Unlike an
+ * ordinary thread, a protothread do not require its own stack.
+ *
+ * Each protothread runs within its own C function. The protothread is
+ * driven by repeated calls to the function that implements the
+ * protothread.
+ *
+ * Example functionality implemented as an explicit state machine in
+ * an event handler:
+ \code
+ static void
+ eventhandler(int state)
+ {
+   switch(state) {
+   case 1:
+     if(condition1) {
+       return;
+     } else {
+       next_state = 2;
+       do_something();
+     }
+     break;
+   case 2:
+     if(condition2) {
+       next_state = 3;
+     } else {
+       next_state = 4;
+       do_something_else();
+     }
+     break;
+   case 3:
+     do_something_else();
+     next_state = 4;
+     break;
+   case 4:
+     next_state = 1;
+     break;
+   }
+ }
+ \endcode
+ *
+ * The same functionality implemented as a protothread:
+ \code
+ static void
+ protothread(struct pt *pt)
+ {
+   PT_START(pt);
 
+   while(condition1) {
+     PT_YIELD(pt);
+   }
+
+   do_something();
+
+   if(condition2) {
+     PT_YIELD(pt);
+   }
+
+   do_something_else();   
+      
+   PT_RESTART(pt);
+ }
+ \endcode
+ *
+ */
+
+/**
+ * \file
+ * Protothreads implementation.
+ * \author
+ * Adam Dunkels <adam@sics.se>
+ *
+ */
 
 #ifndef __PT_H__
 #define __PT_H__
 
-#include "uip.h"
 #include "lc.h"
-#include "uipbuf.h"
 
-struct pt_state {
+struct pt {
   lc_t lc;
-  unsigned char state;
-#define PT_STATE_NONE 0
-#define PT_STATE_ACKED 1
-#define PT_STATE_READ 2
-#define PT_STATE_BLOCKED_NEWDATA 3
-#define PT_STATE_BLOCKED_CLOSE 4
-#define PT_STATE_BLOCKED_SEND 5
-  u16_t tmplen;
-  u8_t *tmpptr;
-  u8_t *readptr;
-  u16_t readlen;
 };
 
-char pt_newdata(struct pt_state *pt);
-char pt_send(struct pt_state *pt);
+#define PT_INIT(pt)				\
+  (pt)->lc = LC_NULL
+  
+#define PT_START(pt)				\
+  do {						\
+    if((pt)->lc == LC_NULL) {			\
+      LC_SET((pt)->lc);				\
+    } else {					\
+      LC_RESUME((pt)->lc);			\
+    } 						\
+  } while(0)
 
-#define PT_INIT(pt) (pt)->lc = LC_NULL
-
-#define PT_CALL(pt, function, args)		\
+#define PT_YIELD(pt)				\
   do {						\
     LC_SET((pt)->lc);				\
-    if(function args) {				\
+    return;					\
+  } while(0)
+
+#define PT_WAIT_COND(pt, condition)	        \
+  do {						\
+    LC_SET((pt)->lc);				\
+    if(condition) {				\
       return;					\
     }						\
   } while(0)
 
 
-#define PT_ACCEPT(type, s, pt, states)					 \
-  do {									 \
-    if(uip_timedout() || uip_closed() || uip_aborted()) { 		 \
-      if(s != NULL && s->pt.state == PT_STATE_BLOCKED_CLOSE) {		 \
-	LC_CALL(s->pt.lc);						 \
-      }									 \
-      memb_free(states, s);                                     	 \
-      tcp_markconn(uip_conn, NULL);                                      \
-      return;								 \
-    } else if(uip_connected()) {					 \
-      s = (type *)memb_alloc(states);					 \
-      if(s == NULL) {							 \
-	uip_abort();							 \
-	return;								 \
-      }									 \
-      tcp_markconn(uip_conn, s);					 \
-      LC_SET(s->pt.lc);							 \
-    } else if(s == NULL) {						 \
-      return;								 \
-    } else {								 \
-      LC_CALL(s->pt.lc);					      	 \
-    }									 \
-    s->pt.state = PT_STATE_NONE;                                         \
-    s->pt.readlen = 0;                                                   \
-  } while(0)							
-
-#define PT_CONNECT(type, s, pt, states)					 \
-  do {									 \
-    if(uip_timedout() || uip_closed() || uip_aborted()) {		 \
-      if(s != NULL && s->pt.state == PT_STATE_BLOCKED_CLOSE) {		 \
-	LC_CALL(s->pt.lc);						 \
-      }									 \
-      memb_free(states, s);                                     	 \
-      tcp_markconn(uip_conn, NULL);                                      \
-      return;								 \
-    } else if(uip_connected()) {					 \
-      s->pt.state = PT_STATE_NONE;                                       \
-    } else {								 \
-      LC_CALL(s->pt.lc);					      	 \
-    }									 \
-  } while(0)							
-
-#define PT_NEWDATA(pt) \
-  PT_CALL(pt, pt_newdata, (pt))
-
-#define PT_SEND(pt, data, datalen)			\
-  do {							\
-    (pt)->tmplen = datalen;			       	\
-    (pt)->tmpptr = data;				\
-    PT_CALL(pt, pt_send, (pt));				\
-  } while(0)
-
-#define PT_SEND_DYNAMIC(pt, dataptr, function, args)	  \
-  do {							  \
-    (pt)->tmpptr = dataptr;				  \
-    (pt)->tmplen = function args;			  \
-    LC_SET((pt)->lc);					  \
-    if((pt)->state == PT_STATE_ACKED || !uip_acked()) {	  \
-      if(uip_rexmit() || (pt)->state != PT_STATE_ACKED) { \
-	function args;					  \
-      }							  \
-      if(pt_send(pt)) {					  \
-	return;						  \
-      }							  \
-    }							  \
-    (pt)->state = PT_STATE_ACKED;                         \
-  } while(0)
-
-#define PT_CLOSED(pt)						\
-  do {								\
-    LC_SET((pt)->lc);						\
-    if(!(uip_closed() || uip_timedout() || uip_aborted())) {	\
-      (pt)->state = PT_STATE_BLOCKED_CLOSE;			\
-      return;      						\
-    }								\
-    (pt)->state = PT_STATE_NONE;				\
-  } while(0)
-
-#define PT_CLOSE(pt)				\
+#define PT_RESTART(pt)				\
   do {						\
-    uip_close();				\
-    PT_CLOSED(pt);				\
+    (pt)->lc = LC_NULL;				\
+    return;					\
   } while(0)
 
-#define PT_READTO(pt, c, uipbuf, buffer, buffersize)	 \
-  do {							 \
-    uipbuf_setup(uipbuf, buffer, buffersize);    	 \
-    do {      						 \
-      if((pt)->readlen == 0) {                           \
-        PT_NEWDATA(pt);					 \
-        (pt)->readptr = (u8_t *)uip_appdata;		 \
-        (pt)->readlen = uip_datalen();			 \
-      }                            			 \
-    } while((uipbuf_bufdata_endmarker(uipbuf, c,	 \
-				      &((pt)->readptr),	 \
-				      &((pt)->readlen)) & \
-	     UIPBUF_FOUND) == 0				 \
-	    && (pt)->readlen > 0);			 \
-  } while(0)
-
-#define PT_READ(pt, len, uipbuf, buffer, buffersize)	\
-  do {							\
-    uipbuf_setup(uipbuf, buffer, buffersize);		\
-    do {						\
-      PT_NEWDATA(pt);					\
-      (pt)->readptr = (u8_t *)uip_appdata;		\
-      (pt)->readlen = uip_datalen();			\
-    } while((uipbuf_bufdata(uipbuf, len,		\
-			    &((pt)->readpt),		\
-			    &((pt)->readlen)) &		\
-	     UIPBUF_FOUND) == 0				\
-	    && (pt)->readlen > 0);			\
-  } while(0)
-
+#define PT_EXIT(pt) PT_INIT(pt)
 
 #endif /* __PT_H__ */
