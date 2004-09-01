c64152b926b5bba9147cb3421bbe8cca0abf64b4
adamdunkels <adamdunkels>
2004-09-01 20:46:41 +0000

    Added LC_INIT, LC_END

--- contiki-cc65/lib/lc.h
+++ contiki-cc65/lib/lc.h
@@ -5,14 +5,18 @@
 #ifndef __LC_H__
 #define __LC_H__
 
+#include "cc.h"
+
 typedef void * lc_t;
 
 lc_t lc_get(void);
-void __fastcall__ lc_call(lc_t s);
+void __fastcall__ lc_resume(lc_t s);
 
 #define LC_SET(s) s = lc_get()
-#define LC_CALL(s) lc_call(s) 
+#define LC_RESUME(s) lc_resume(s)
 
-#define LC_NULL NULL
+#define LC_INIT(s) s = NULL
+
+#define LC_END(s)
 
 #endif /* __LC_H__ */
