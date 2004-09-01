86ec98fd1062d43c6d5ecb99b4a14a1e3ba8a5bc
adamdunkels <adamdunkels>
2004-09-01 20:48:04 +0000

    Removed advertising clause

--- contiki-c64/apps/wget.c
+++ contiki-c64/apps/wget.c
@@ -11,10 +11,7 @@
  *    copyright notice, this list of conditions and the following
  *    disclaimer in the documentation and/or other materials provided
  *    with the distribution. 
- * 3. All advertising materials mentioning features or use of this
- *    software must display the following acknowledgement:
- *        This product includes software developed by Adam Dunkels. 
- * 4. The name of the author may not be used to endorse or promote
+ * 3. The name of the author may not be used to endorse or promote
  *    products derived from this software without specific prior
  *    written permission.  
  *
@@ -32,7 +29,7 @@
  *
  * This file is part of the Contiki desktop environment
  *
- * $Id: wget.c,v 1.9 2004/07/04 18:33:07 adamdunkels Exp $
+ * $Id: wget.c,v 1.10 2004/09/01 20:48:04 adamdunkels Exp $
  *
  */
 
