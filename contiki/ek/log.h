de8082fc853f4c6c4f70b82e519c15adc1efdb79
adamdunkels <adamdunkels>
2004-09-03 09:49:00 +0000

    Added const qualfier to keep cc65 happy

--- contiki/ek/log.h
+++ contiki/ek/log.h
@@ -4,7 +4,7 @@
 #include "log-conf.h"
 
 #if LOG_CONF_ENABLED
-void log_message(char *part1, char *part2);
+void log_message(const char *part1, const char *part2);
 #else /* LOG_CONF_ENABLED */
 #define log_message(p1, p2)
 #endif /* LOG_CONF_ENABLED */
