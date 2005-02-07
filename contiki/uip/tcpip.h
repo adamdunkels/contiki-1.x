6f737b74d899f7555b70466e99dbb4a8a25df119
adamdunkels <adamdunkels>
2005-02-07 07:07:24 +0000

    Added documentation to uIP/Contiki API

--- contiki/uip/tcpip.h
+++ contiki/uip/tcpip.h
@@ -1,3 +1,28 @@
+/**
+ * \defgroup tcpip The Contiki/uIP interface
+ * @{
+ *
+ * TCP/IP support in Contiki is implemented using the uIP TCP/IP
+ * stack. For sending and receiving data, Contiki uses the functions
+ * provided by the uIP module, but Contiki adds a set of functions for
+ * connection management. The connection management functions make
+ * sure that the uIP TCP/IP connections are connected to the correct
+ * process.
+ *
+ * Contiki also includes an optional socket-like library for TCP
+ * connections.
+ *
+ * \sa \ref uip "The uIP TCP/IP stack"
+ * \sa \ref socket "Socket-like library"
+ *
+ */
+
+/**
+ * \file
+ * Header for the Contiki/uIP interface.
+ * \author
+ * Adam Dunkels <adam@sics.se>
+ */
 /*
  * Copyright (c) 2004, Swedish Institute of Computer Science.
  * All rights reserved. 
@@ -30,7 +55,7 @@
  * 
  * Author: Adam Dunkels <adam@sics.se>
  *
- * $Id: tcpip.h,v 1.4 2004/09/17 20:49:49 adamdunkels Exp $
+ * $Id: tcpip.h,v 1.5 2005/02/07 07:07:24 adamdunkels Exp $
  */
 #ifndef __TCPIP_H__
 #define __TCPIP_H__
@@ -57,11 +82,67 @@ EK_PROCESS_INIT(tcpip_init, arg);
      
 void tcpip_uipcall(void);
 
+/**
+ * Mark a TCP connection with the current process.
+ *
+ * This function ties a TCP connection with the current process. Each
+ * TCP connection must be tied to a process in order for the process
+ * to be able to receive and send data. Additionally, this function
+ * can add a pointer with connection state to the connection.
+ *
+ * \param conn A pointer to the TCP connection.
+ *
+ * \param appstate An opaque pointer that will be passed to the
+ * process whenever an event occurs on the connection.
+ *
+ */
 void tcp_markconn(struct uip_conn *conn,
 		  void *appstate);
 
+/**
+ * Open a TCP port.
+ *
+ * This function opens a TCP port for listening. When a TCP connection
+ * request occurs for the port, the process will be sent a tcpip_event
+ * with the new connection request.
+ *
+ * \note Port numbers must always be given in network byte order. The
+ * functions HTONS() and htons() can be used to convert port numbers
+ * from host byte order to network byte order.
+ *
+ * Example
+ \code
+ static void init_ports(void) {
+   tcp_listen(HTONS(80));
+   tcp_listen(HTONS(81));
+ }
+ \endcode
+ *
+ * \param port The port number in network byte order. 
+ *
+ */
 void tcp_listen(u16_t port);
 
+/**
+ * Close a listening TCP port.
+ *
+ * This function closes a listening TCP port.
+ *
+ * \note Port numbers must always be given in network byte order. The
+ * functions HTONS() and htons() can be used to convert port numbers
+ * from host byte order to network byte order.
+ *
+ * Example
+ \code
+ static void close_ports(void) {
+   tcp_unlisten(HTONS(80));
+   tcp_unlisten(HTONS(81));
+ }
+ \endcode
+ *
+ * \param port The port number in network byte order. 
+ *
+ */
 void tcp_unlisten(u16_t port);
 
 struct uip_conn *tcp_connect(u16_t *ripaddr, u16_t port,
